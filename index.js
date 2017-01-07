95ffe98be3f5a44dd72d78a69e173449e267fbf3
Juha Lindstedt <juha@pakastin.fi>
2017-01-07 14:12:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const style = document.createElement('p').style;
+const memoized = {};
+
+export function prefix (param) {
+  if (typeof memoized[param] !== 'undefined') {
+    return memoized[param];
+  }
+
+  if (typeof style[param] !== 'undefined') {
+    memoized[param] = param;
+    return param;
+  }
+
+  const camelCase = param[0].toUpperCase() + param.slice(1);
+  const prefixes = ['webkit', 'moz', 'ms', 'o'];
+  let test;
+
+  for (let i = 0, len = prefixes.length; i < len; i++) {
+    test = prefixes[i] + camelCase;
+
+    if (typeof style[test] !== 'undefined') {
+      memoized[param] = test;
+      return test;
+    }
+  }
+}
